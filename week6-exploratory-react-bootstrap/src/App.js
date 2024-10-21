c933d77e39fa555a1d3524261334a37910c37737
sharondsouza <sharondsouza26@gmail.com>
2024-10-21 12:47:49 -0400

    Adding icons to Nav items

--- week6-exploratory-react-bootstrap/src/App.js
+++ week6-exploratory-react-bootstrap/src/App.js
@@ -1,21 +1,30 @@
 import './App.css';
 import { Nav, Navbar } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import { BiSolidDashboard } from "react-icons/bi";
+import { FaUsers } from "react-icons/fa";
+import { SiSimpleanalytics } from "react-icons/si";
+import { PiPackageFill } from "react-icons/pi";
+import { RiSettings5Fill } from "react-icons/ri";
 
 function App() {
   return (
-    <div className="flex-column">
+    <div className="App" >
       <Navbar expand="lg" className="justify-content-left">
         <Navbar.Brand className="flex-column" href="#home">Navigation Sidebar</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="flex-column">
-          <Nav.Link href="#home">Dashboard</Nav.Link>
-          <Nav.Link href="#link">Users</Nav.Link>
-          <Nav.Link href="#link">Analytics</Nav.Link>
-          <Nav.Link href="#link">Orders</Nav.Link>
-          <Nav.Link href="#link">Settings</Nav.Link>
+          <Nav.Link href="#home">
+          <BiSolidDashboard /> Dashboard</Nav.Link>
+          <Nav.Link href="#link">
+          <FaUsers /> Users</Nav.Link>
+          <Nav.Link href="#link">
+          <SiSimpleanalytics /> Analytics</Nav.Link>
+          <Nav.Link href="#link">
+          <PiPackageFill/> Orders</Nav.Link>
+          <Nav.Link href="#link">
+          <RiSettings5Fill /> Settings</Nav.Link>
             </Nav>
             </Navbar.Collapse>
             </Navbar>
