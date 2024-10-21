401d2da9eaae7de6b659b4a1855e9c204856ebff
sharondsouza <sharondsouza26@gmail.com>
2024-10-21 12:31:05 -0400

    creating the base side bar

--- week6-exploratory-react-bootstrap/src/App.js
+++ week6-exploratory-react-bootstrap/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { Nav, Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex-column">
+      <Navbar expand="lg" className="justify-content-left">
+        <Navbar.Brand className="flex-column" href="#home">Navigation Sidebar</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="flex-column">
+          <Nav.Link href="#home">Dashboard</Nav.Link>
+          <Nav.Link href="#link">Users</Nav.Link>
+          <Nav.Link href="#link">Analytics</Nav.Link>
+          <Nav.Link href="#link">Orders</Nav.Link>
+          <Nav.Link href="#link">Settings</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+            </Navbar>
     </div>
   );
 }
