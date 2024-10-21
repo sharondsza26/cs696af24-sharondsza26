e314269a70bf92f505305ff37f9f6dee5e4dab44
sharondsouza <sharondsouza26@gmail.com>
2024-10-21 16:27:46 -0400

    Navigation Sidebar

--- week6-exploratory-react-bootstrap/src/App.js
+++ week6-exploratory-react-bootstrap/src/App.js
@@ -7,12 +7,23 @@ import { SiSimpleanalytics } from "react-icons/si";
 import { PiPackageFill } from "react-icons/pi";
 import { RiSettings5Fill } from "react-icons/ri";
 
+import { FaBell } from 'react-icons/fa';  // Notification icon
+import { AiOutlineSearch } from 'react-icons/ai';  // Search icon
+import { Dropdown } from 'react-bootstrap'; // Dropdown for user profile
+
 function App() {
   return (
-    <div className="App" >
-      <Navbar expand="lg" className="justify-content-left">
-        <Navbar.Brand className="flex-column" href="#home">Navigation Sidebar</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    // Outer App
+    <div className="App" > 
+
+      <Navbar expand="lg" >
+       <div className='NavigationSidebar'>
+         <Navbar.Brand href="#home">Navigation Sidebar</Navbar.Brand>
+         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+         <div className='navigation-content'>
+
+
+        {/* Nav Content */}
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="flex-column">
           <Nav.Link href="#home">
@@ -27,8 +38,21 @@ function App() {
           <RiSettings5Fill /> Settings</Nav.Link>
             </Nav>
             </Navbar.Collapse>
+            </div>
+         </div>
+           
+      
             </Navbar>
-    </div>
+
+        <div className='Footer'>
+          <p> Footer </p>
+        </div>
+
+            </div> //  Outer app
+
+
+
+        
   );
 }
 
